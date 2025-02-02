ff83028ebc8d4131f4c93b1592ef6d5ba0d45bf3
anuragsknsits <anuragsknsits@gmail.com>
2025-02-02 23:14:41 +0530

    - Delete the unused file.

--- src/main/java/com/codewithanurag/authentication/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.codewithanurag.authentication.service;
-
-import com.codewithanurag.authentication.model.SignUp;
-
-public interface AuthService {
-    String signUp(SignUp signUp);
-
-    String login(String userName, String password);
-}
